03bbdbc122f55ba1702c6c3dd6c1f378914aaa82
Jon Bushby <jon.bushby@outlook.com>
2018-07-23 11:50:58 -0400

    Added WPF Example

---
+++ PCLPrintExample/PCLPrintExample.WPF/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.WPF;
+
+namespace PCLPrintExample.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : FormsApplicationPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Forms.Init();
+            LoadApplication(new PCLPrintExample.App());
+
+        }
+    }
+}
---
+++ PCLPrintExample/PCLPrintExample.WPF/Print.cs
@@ -0,0 +1,53 @@
+﻿using PCLPrintExample.WPF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Print))]
+namespace PCLPrintExample.WPF
+{
+    public class Print : IPrint
+    {
+        void IPrint.Print(byte[] content)
+        {
+            PrintDialog printDialog = new PrintDialog();
+            if (printDialog.ShowDialog() == true)
+            {
+
+                var bi = ToImage(content);
+
+                var vis = new DrawingVisual();
+                var dc = vis.RenderOpen();
+                dc.DrawImage(bi, new Rect { Width = bi.Width, Height = bi.Height });
+                dc.Close();
+
+                var pdialog = new PrintDialog();
+                if (pdialog.ShowDialog() == true)
+                {
+                    pdialog.PrintVisual(vis, "My Image");
+                }
+
+            }
+                 }
+
+        public BitmapImage ToImage(byte[] array)
+        {
+            using (var ms = new System.IO.MemoryStream(array))
+            {
+                var image = new BitmapImage();
+                image.BeginInit();
+                image.CacheOption = BitmapCacheOption.OnLoad; // here
+                image.StreamSource = ms;
+                image.EndInit();
+                return image;
+            }
+        }
+    }
+}
